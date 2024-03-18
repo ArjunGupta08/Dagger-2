ddfefd6215c8644377acfca8e9eddbdf2f3b967e
Arjun Gupta <arjungupta0817@gmail.com>
2024-03-18 17:12:11 +0530

    Qualifiers and custom annotations

--- app/src/main/java/com/arjungupta08/dagger2/NotificationServiceModule.kt
+++ app/src/main/java/com/arjungupta08/dagger2/NotificationServiceModule.kt
@@ -2,11 +2,19 @@ package com.arjungupta08.dagger2
 
 import dagger.Module
 import dagger.Provides
+import javax.inject.Named
 
 @Module
 class NotificationServiceModule {
+    @MessageQualifier
     @Provides
-    fun getEmailService() : NotificationService {
-        return EmailService()
+    fun getMessageService() : NotificationService {
+        return MessageService()
+    }
+
+    @Named("email")
+    @Provides
+    fun getEmailService(emailService: EmailService) : NotificationService {
+        return emailService
     }
 }
